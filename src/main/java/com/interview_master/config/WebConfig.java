62de2e7f99d75cf640c339506e816e5cc37b8ff5
nick <andantej281@gmail.com>
2024-08-09 20:06:26 +0900

    chore: 문법 오류 해결

--- src/main/java/com/interview_master/config/WebConfig.java
+++ src/main/java/com/interview_master/config/WebConfig.java
@@ -21,6 +21,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
-            .excludePathPatterns("/api/auth/*")
+            .excludePathPatterns("/api/auth/*");
     }
 }
