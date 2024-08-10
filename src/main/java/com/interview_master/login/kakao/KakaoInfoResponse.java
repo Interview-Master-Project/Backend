b5ff6c5f71e0b018e1126c996866be333e47e7da
nick <andantej281@gmail.com>
2024-08-10 16:44:12 +0900

    feat: kakao login 개발 완료

---
+++ src/main/java/com/interview_master/login/kakao/KakaoInfoResponse.java
@@ -0,0 +1,43 @@
+package com.interview_master.login.kakao;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.interview_master.login.OAuthInfoResponse;
+import com.interview_master.login.OAuthProvider;
+import lombok.Getter;
+
+@Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class KakaoInfoResponse implements OAuthInfoResponse {
+
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoAccount {
+        private KakaoProfile profile;
+        private String email;
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class KakaoProfile {
+        private String nickname;
+    }
+
+    @Override
+    public String getEmail() {
+        return kakaoAccount.email;
+    }
+
+    @Override
+    public String getNickname() {
+        return kakaoAccount.profile.nickname;
+    }
+
+    @Override
+    public OAuthProvider getOAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+}
--- src/main/java/com/interview_master/ui/AuthController.java
+++ src/main/java/com/interview_master/ui/AuthController.java
@@ -1,9 +1,9 @@
 package com.interview_master.ui;
 
-import static com.interview_master.common.constant.SessionConst.LOGIN_USER;
-
 import com.interview_master.domain.user.User;
-import com.interview_master.login.NaverLoginParams;
+import com.interview_master.login.OAuthLoginParams;
+import com.interview_master.login.kakao.KakaoLoginParams;
+import com.interview_master.login.naver.NaverLoginParams;
 import com.interview_master.login.OAuthLoginService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.interview_master.common.constant.SessionConst.LOGIN_USER;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -22,19 +24,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final OAuthLoginService oAuthLoginService;
 
-//    @PostMapping("/kakao")
-//    public ResponseEntity<Object> loginKakao(@RequestBody KakaoLoginParams params) {
-//        return ResponseEntity.ok(oAuthLoginService.login(params));
-//    }
+    @PostMapping("/kakao")
+    public ResponseEntity<Object> loginKakao(@RequestBody KakaoLoginParams params, HttpServletRequest request) {
+        log.info("{} login params {}", "Kakao", params.getAuthorizationCode());
+        User user = login(params, request);
+
+        return ResponseEntity.ok(user);
+    }
 
     @PostMapping("/naver")
     public ResponseEntity<User> loginNaver(@RequestBody NaverLoginParams params, HttpServletRequest request) {
-        log.info("login params {}, {}", params.getAuthorizationCode(), params.getState());
+        log.info("{} login params {}, {}", "Naver", params.getAuthorizationCode(), params.getState());
+        User user = login(params, request);
+
+        return ResponseEntity.ok(user);
+    }
+
+    private User login(OAuthLoginParams params, HttpServletRequest request) {
         User user = oAuthLoginService.login(params);
 
         HttpSession session = request.getSession();
         session.setAttribute(LOGIN_USER, user);
-        
-        return ResponseEntity.ok(user);
+        return user;
     }
 }
