5dc544348e881f478cf438be2cfd41a62548e29e
nick <andantej281@gmail.com>
2024-08-13 22:25:00 +0900

    chore: 변수명 수정

--- src/main/java/com/interview_master/ui/QuizController.java
+++ src/main/java/com/interview_master/ui/QuizController.java
@@ -33,8 +33,8 @@ public class QuizController {
 
     @QueryMapping
     public List<Quiz> getMyQuiz() {
-        Long userId = ExtractUserId.extractUserIdFromContextHolder();
-        log.info("======== getMyQuiz : {}", userId);
-        return quizService.findByUserId(userId);
+        Long currentUserId = ExtractUserId.extractUserIdFromContextHolder();
+        log.info("======== getMyQuiz : {}", currentUserId);
+        return quizService.findByUserId(currentUserId);
     }
 }
