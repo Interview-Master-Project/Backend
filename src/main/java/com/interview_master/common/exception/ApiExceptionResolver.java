f0619042a298d4977f090cbbdeb023cb5253df9f
nick <andantej281@gmail.com>
2024-08-14 17:24:40 +0900

    feat: DTO의 Validation 실패 시, 예외 처리하는 로직 구현

--- src/main/java/com/interview_master/common/exception/ApiExceptionResolver.java
+++ src/main/java/com/interview_master/common/exception/ApiExceptionResolver.java
@@ -6,6 +6,8 @@ import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Component
 public class ApiExceptionResolver extends DataFetcherExceptionResolverAdapter {
@@ -20,7 +22,17 @@ public class ApiExceptionResolver extends DataFetcherExceptionResolverAdapter {
                     .location(env.getField().getSourceLocation())
                     .build();
         }
+        else if (ex instanceof MethodArgumentNotValidException) {
+            FieldError fieldError = ((MethodArgumentNotValidException) ex).getBindingResult().getFieldError();
+            String errorMessage = (fieldError != null) ? fieldError.getDefaultMessage() : "Validation error";
 
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.ValidationError)
+                    .message(errorMessage)
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        }
         return null;
     }
 }
