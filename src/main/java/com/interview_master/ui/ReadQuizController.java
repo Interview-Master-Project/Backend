2b52a6f37b9b83bd379380c1b4cb69e5b00f365d
nick <andantej281@gmail.com>
2024-08-16 14:03:37 +0900

    feat: 조회 / 수정 컨트롤러 분리

---
+++ src/main/java/com/interview_master/ui/ReadQuizController.java
@@ -0,0 +1,35 @@
+package com.interview_master.ui;
+
+import com.interview_master.application.ReadQuizService;
+import com.interview_master.domain.quiz.Quiz;
+import com.interview_master.ui.response.QuizWithCollectionAndResults;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ReadQuizController {
+
+    private final ReadQuizService readQuizService;
+
+    @QueryMapping
+    public Quiz quizById(@Argument Long id) {
+        return readQuizService.findById(id);
+    }
+
+    @QueryMapping
+    public List<QuizWithCollectionAndResults> getMyQuiz() {
+        return readQuizService.findMyQuizWithCollectionNameAndResults();
+    }
+
+    @QueryMapping
+    public List<QuizWithCollectionAndResults> quizByCollectionId(@Argument Long id) {
+        return readQuizService.findByCollectionId(id);
+    }
+}
