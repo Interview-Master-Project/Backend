93dc868d3d8aae9493b516cc334564345ca04e61
nick <andantej281@gmail.com>
2024-08-23 14:43:38 +0900

    feat: 예외 처리 추가

--- src/main/java/com/interview_master/common/exception/ApiExceptionResolver.java
+++ src/main/java/com/interview_master/common/exception/ApiExceptionResolver.java
@@ -4,10 +4,14 @@ import graphql.ErrorType;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.util.stream.Collectors;
 
 @Component
 public class ApiExceptionResolver extends DataFetcherExceptionResolverAdapter {
@@ -25,6 +29,17 @@ public class ApiExceptionResolver extends DataFetcherExceptionResolverAdapter {
         else if (ex instanceof MethodArgumentNotValidException) {
             FieldError fieldError = ((MethodArgumentNotValidException) ex).getBindingResult().getFieldError();
             String errorMessage = (fieldError != null) ? fieldError.getDefaultMessage() : "Validation error";
+
+            return GraphqlErrorBuilder.newError()
+                    .errorType(ErrorType.ValidationError)
+                    .message(errorMessage)
+                    .path(env.getExecutionStepInfo().getPath())
+                    .location(env.getField().getSourceLocation())
+                    .build();
+        } else if (ex instanceof ConstraintViolationException) {
+            String errorMessage = ((ConstraintViolationException) ex).getConstraintViolations().stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.joining(", "));
 
             return GraphqlErrorBuilder.newError()
                     .errorType(ErrorType.ValidationError)
