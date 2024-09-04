cded757fe1ae87978cd1438231ace95355348a80
nick <andantej281@gmail.com>
2024-09-04 18:39:09 +0900

    fix: response에 @Data 추가하기

--- src/main/java/com/interview_master/ui/response/LoginRes.java
+++ src/main/java/com/interview_master/ui/response/LoginRes.java
@@ -1,7 +1,13 @@
 package com.interview_master.ui.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class LoginRes {
     private String nickname;
