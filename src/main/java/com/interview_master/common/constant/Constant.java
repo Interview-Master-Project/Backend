28780f0009a2e06cdcd494cb0228a48ede71941d
nick <andantej281@gmail.com>
2024-09-08 15:39:20 +0900

    init: develop branch 생성

--- src/main/java/com/interview_master/common/constant/Constant.java
+++ src/main/java/com/interview_master/common/constant/Constant.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Constant {
+
     public static final String TOKEN_KEY = "authorization-token";
 
     public static final String USER_ID = "userID";
