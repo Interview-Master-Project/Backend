8fcf25e706bdbbe17e57fb138a8fa6e05f7a2eca
nick <andantej281@gmail.com>
2024-09-12 21:02:36 +0900

    feat: QuizGarden 조회 기능 개발 완료 - startDate ~ endDate까지 일별 푼 퀴즈 개수 리턴 - 일별 요일 인덱스와 startDate로 부터 몇 주차인지 리턴

---
+++ src/main/java/com/interview_master/dto/QuizLog.java
@@ -0,0 +1,9 @@
+package com.interview_master.dto;
+
+import java.time.LocalDate;
+
+public interface QuizLog {
+
+    LocalDate getDate();
+    Integer getQuizzesSolved();
+}
--- src/main/java/com/interview_master/ui/MyPageController.java
+++ src/main/java/com/interview_master/ui/MyPageController.java
@@ -2,14 +2,19 @@ package com.interview_master.ui;
 
 import com.interview_master.application.MyPageService;
 import com.interview_master.application.UserProfileService;
+import com.interview_master.application.UserQuizAttemptService;
 import com.interview_master.domain.user.User;
 import com.interview_master.dto.CollectionPage;
 import com.interview_master.dto.MyPage;
+import com.interview_master.dto.QuizGarden;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,17 +22,22 @@ import org.springframework.stereotype.Controller;
 public class MyPageController {
     private final UserProfileService userProfileService;
     private final MyPageService collectionService;
+    private final UserQuizAttemptService userQuizAttemptService;
 
     @QueryMapping
-    public MyPage myPage(@Argument Long userId, @Argument Integer offset, @Argument Integer limit) {
-        log.info("========== my page\t userId: {}, offset: {}, limit: {}", userId, offset, limit);
+    public MyPage myPage(@Argument Long userId, @Argument Integer offset, @Argument Integer limit,
+                         @Argument String startDate, @Argument String endDate) {
+        log.info("========== my page\t userId: {}, offset: {}, limit: {}, startDate : {}, endDate : {}",
+                userId, offset, limit, startDate, endDate);
 
         User profile = userProfileService.getProfile(userId);
         CollectionPage collectionPage = collectionService.userCollections(userId, offset, limit);
+        List<QuizGarden> quizGardens = userQuizAttemptService.getQuizGardens(LocalDate.parse(startDate), LocalDate.parse(endDate), userId);
 
         return MyPage.builder()
                 .user(profile)
                 .collectionPage(collectionPage)
+                .quizGardens(quizGardens)
                 .build();
     }
 
