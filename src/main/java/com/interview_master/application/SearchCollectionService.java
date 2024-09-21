f9fe6e6545269c87bab1cf2c1ebf2105440de0dc
nick <andantej281@gmail.com>
2024-09-21 21:22:58 +0900

    feat: 컬렉션 검색 graphql, Controller 개발 완료 - Service layer, DB Query 개발 필요

---
+++ src/main/java/com/interview_master/application/SearchCollectionService.java
@@ -0,0 +1,9 @@
+package com.interview_master.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SearchCollectionService {
+}
