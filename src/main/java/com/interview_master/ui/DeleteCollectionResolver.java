c38839ad0350234030451b4cd92389aaa4db86b3
nick <andantej281@gmail.com>
2024-09-26 19:21:58 +0900

    chore: 파일명 controller -> resolver로 수정

---
+++ src/main/java/com/interview_master/ui/DeleteCollectionResolver.java
@@ -0,0 +1,30 @@
+package com.interview_master.ui;
+
+import static com.interview_master.util.GraphQLAuthUtils.validateUserAuthContext;
+
+import com.interview_master.application.UpsertCollectionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.ContextValue;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class DeleteCollectionResolver {
+
+  private final UpsertCollectionService upsertCollectionService;
+
+  @MutationMapping
+  public String deleteCollection(@Argument Long collectionId,
+      @ContextValue(required = false) Long userId,
+      @ContextValue(required = false) String authError) {
+
+    validateUserAuthContext(userId, authError);
+
+    upsertCollectionService.deleteCollection(collectionId, userId);
+
+    return "success";
+  }
+
+}
