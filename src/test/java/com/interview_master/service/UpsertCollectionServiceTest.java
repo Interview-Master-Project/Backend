18effa25fecdffbac8fe9b440b27c796d7eb1003
nick <andantej281@gmail.com>
2024-10-28 20:23:13 +0900

    feat: 좋아요 취소 기능 개발 완료 - collections_likes 테이블에서 liked = false인 데이터들은 스케줄링을 통해서 삭제할 예정 - 동시성 test 통과(동시에 100명이 한 컬렉션에 대해서 좋아요 취소 요청하는 테스트 -> 정상적으로 좋아요 개수  -100 되는지)

--- src/test/java/com/interview_master/service/UpsertCollectionServiceTest.java
+++ src/test/java/com/interview_master/service/UpsertCollectionServiceTest.java
@@ -67,5 +67,47 @@ public class UpsertCollectionServiceTest {
     assertEquals(numberOfUsers, collection.getLikes());
   }
 
+  @Test
+  public void unlike() throws InterruptedException {
+    int numberOfUsers = 100;
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    Long targetCollectionId = 10L;
+    CountDownLatch latch = new CountDownLatch(numberOfUsers);
+
+    // 테스트용 사용자 ID 리스트 생성 (1부터 100까지)
+    List<Long> userIds = IntStream.rangeClosed(1, numberOfUsers)
+        .mapToObj(Long::valueOf)
+        .toList();
+
+    for (Long userId : userIds) {
+      executorService.submit(() -> {
+        try {
+          boolean success = false;
+          while (!success) {
+            try {
+              upsertCollectionService.unlikeCollection(targetCollectionId, userId);
+              success = true;
+            } catch (Exception e) {
+              try {
+                Thread.sleep(50); // 잠깐 대기 후 재시도
+              } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+              }
+            }
+          }
+        } finally {
+          latch.countDown();
+        }
+      });
+    }
+
+    latch.await();
+    executorService.shutdown();
+
+    Collection collection = collectionRepository.findByIdAndIsDeletedFalse(targetCollectionId)
+        .orElseThrow(() -> new RuntimeException("Collection not found"));
+
+    assertEquals(0, collection.getLikes());
+  }
 
 }
